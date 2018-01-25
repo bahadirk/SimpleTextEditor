5b69d680b70db42347d9f04ec2306416e4d35abf
bahadirk <bahadir.kirdan@tum.de>
2018-01-25 09:22:14 +0100

    Fixed empty else case

--- src/main/java/gui/LineArea.java
+++ src/main/java/gui/LineArea.java
@@ -6,57 +6,54 @@ import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
 import javax.swing.text.StyledDocument;
 
-public class LineArea extends JTextArea implements CaretListener{
- 
+public class LineArea extends JTextArea implements CaretListener {
+
 	TextArea textArea;
 	StyledDocument document;
-	
-	public LineArea(){
+
+	public LineArea() {
 		textArea = new TextArea();
-		
+
 		setSize(300, 600);
 		setEditable(false);
-	
+
 	}
-	
 
 	public void caretUpdate(CaretEvent e) {
-		displaySelectionInfo(e.getDot(), (TextArea) e.getSource());	
+		displaySelectionInfo(e.getDot(), (TextArea) e.getSource());
 	}
 
 	private void displaySelectionInfo(final int dot, final TextArea textArea) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				
+
 				int textAreaRowNumber = textArea.getLineCount();
 				int lineAreaRowNumber = getLineCount();
-				
-				if(textAreaRowNumber == lineAreaRowNumber){
-					if(lineAreaRowNumber == 1){
+
+				if (textAreaRowNumber == lineAreaRowNumber) {
+					if (lineAreaRowNumber == 1) {
 						setText("1");
-					}else {
-						
 					}
-				}else{
+				} else {
 					String currentText = getText();
-					//String textAreaCurrentText = textArea.getText();
-					
-					if(textAreaRowNumber > lineAreaRowNumber){
-						for(int i = lineAreaRowNumber; i<textAreaRowNumber; i++){
-							currentText += "\n" + Integer.toString(i+1);
+					// String textAreaCurrentText = textArea.getText();
+
+					if (textAreaRowNumber > lineAreaRowNumber) {
+						for (int i = lineAreaRowNumber; i < textAreaRowNumber; i++) {
+							currentText += "\n" + Integer.toString(i + 1);
 							setText(currentText);
 						}
-					}else {
-						
+					} else {
+
 					}
 				}
-			
+
 			}
 		});
 	}
 
-	public CaretListener getRowPanelCaretListener(){
-		return (CaretListener) this;
+	public CaretListener getRowPanelCaretListener() {
+		return this;
 	}
-	
+
 }
