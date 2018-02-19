e4fbae2aefb82ab1c08d82703843167a9f4cb515
bahadirk <bahadir.kirdan@tum.de>
2018-02-19 13:25:00 +0100

    Added instant

--- src/main/java/gui/InfoPanel.java
+++ src/main/java/gui/InfoPanel.java
@@ -8,22 +8,23 @@ import javax.swing.event.CaretListener;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Utilities;
 
-public class InfoPanel extends JPanel{
-	
+public class InfoPanel extends JPanel {
+
+	static final int LOGGER = 15;
+
 	JLabel rowAndColLabel;
 	CaretListener rowAndColLabelListener;
-	
-	public InfoPanel(){
+
+	public InfoPanel() {
 		rowAndColLabel = new RowAndColLabel("Line 1, Column 1");
 		rowAndColLabelListener = (CaretListener) rowAndColLabel;
 		add(rowAndColLabel);
 	}
-	
-	public CaretListener getRowAndColLabelListener(){
+
+	public CaretListener getRowAndColLabelListener() {
 		return (CaretListener) rowAndColLabel;
 	}
 }
-
 
 class RowAndColLabel extends JLabel implements CaretListener {
 	public RowAndColLabel(String label) {
@@ -38,17 +39,17 @@ class RowAndColLabel extends JLabel implements CaretListener {
 	protected void displaySelectionInfo(final int dot, final TextArea textArea) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				
+
 				String row = Integer.toString(textArea.getLineCount());
 				String column = getColumn(dot, textArea);
 				setText("Line: " + row + " Column: " + column);
 			}
 		});
 	}
-	
+
 	private String getColumn(int pos, TextArea textArea) {
 		try {
-			return Integer.toString(pos-Utilities.getRowStart(textArea, pos)+1);
+			return Integer.toString(pos - Utilities.getRowStart(textArea, pos) + 1);
 		} catch (BadLocationException e) {
 			e.printStackTrace();
 		}
